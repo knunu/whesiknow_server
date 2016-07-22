01691d7228a5195100a5cefda73b3a4fe15e74cb
Knunu <burette@hanyang.ac.kr>
2016-07-22 18:32:12 +0900

    config updated

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://knunu.ml/whesiknow/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
