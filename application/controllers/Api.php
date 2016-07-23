fb36bbd0b28e30cb5bc27ea698065f798125be6a
Knunu <burette@hanyang.ac.kr>
2016-07-24 05:23:51 +0900

    api post modified

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -75,7 +75,7 @@ class Api extends REST_Controller {
 
         if ($this->api_model->post(TABLE, $new_value)) {
             $this->response(
-                $this->api_model->get(TABLE, $new_value, 'id, email, name')
+                $this->api_model->get(TABLE, $new_value, 'id, email, name')->row()
             , REST_Controller::HTTP_OK);
         } else {
             $this->response([
